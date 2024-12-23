83ec7ef1021724d986801d52019daaf8cf481efd
RosDeeper <hoanggg237@gmail.com>
2024-12-24 00:28:13 +0700

    basic crowdfunding smart contract & create tier

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -8,6 +8,27 @@ contract Crowdfunding {
     uint256 public deadline;
     address public owner;
 
+    enum CampaignState { Active, Successful, Failed }
+    CampaignState public state;
+
+    struct Tier {
+        string name;
+        uint256 amount;
+        uint256 backers;
+    }
+
+    Tier[] public tiers;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner.");
+        _;
+    }
+
+    modifier campaignOpen() {
+        require(state == CampaignState.Active, "Campaign is not active.");
+        _;
+    }
+
     constructor(
         string memory _name,
         string memory _description,
@@ -19,16 +40,45 @@ contract Crowdfunding {
         goal = _goal;
         deadline = block.timestamp + (_duratyionInDays * 1 days);
         owner = msg.sender;
+        state = CampaignState.Active;
     }
 
-    function fund() public payable {
-        require(msg.value > 0, "Must fund amount greater than 0.");
-        require(block.timestamp < deadline, "Campaign has ended");
+    function checkAndUpdateCampaignState() internal {
+        if(state == CampaignState.Active) {
+            if(block.timestamp >= deadline) {
+                state = address(this).balance >= goal ? CampaignState.Successful : CampaignState.Failed;
+            } else {
+                state = address(this).balance >= goal ? CampaignState.Successful : CampaignState.Active;
+            }
+        }
     }
 
-    function withdraw() public {
-        require(msg.sender == owner, "Only the owner can withdraw.");
-        require(address(this).balance >= goal, "Goal had not been reached.");
+    function fund(uint256 _tierIndex) public payable campaignOpen {
+        require(_tierIndex < tiers.length, "Invalid tier.");
+        require(msg.value == tiers[_tierIndex].amount, "Incorrect amount.");
+
+        tiers[_tierIndex].backers++;
+
+        checkAndUpdateCampaignState();
+    }
+
+    function addTier(
+        string memory _name,
+        uint256 _amount
+    ) public onlyOwner {
+        require(_amount > 0, "Amount must be greater than 0.");
+        tiers.push(Tier(_name, _amount, 0));
+    }
+
+    function removeTier(uint256 _index) public onlyOwner {
+        require(_index < tiers.length, "Tier does not exist.");
+        tiers[_index] = tiers[tiers.length - 1];
+        tiers.pop();
+    }
+
+    function withdraw() public onlyOwner {
+        checkAndUpdateCampaignState();
+        require(state == CampaignState.Successful, "Campaign not successful.");
 
         uint256 balance = address(this).balance;
         require(balance >= 0, "No balance to withdraw.");
